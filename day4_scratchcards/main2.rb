cc27a7a132f2f2702d99d6dc270ee69841a29eb1
Marcin Skłodowski <marcinsklodowski@gmail.com>
2023-12-04 09:13:44 +0100

    Update main2.rb

--- day4_scratchcards/main2.rb
+++ day4_scratchcards/main2.rb
@@ -2,21 +2,12 @@ require 'pry'
 
 lines = File.open("input", "r")
             .readlines.map(&:chomp)
-            .map { |a| a.split(":").last }.map { |a| a.split("|") }
+            .map { |a| a.split(":").last }
+            .map { |a| a.split("|") }
+            .map { |line| [1, (line[0].split(" ").map(&:to_i) & line[1].split(" ").map(&:to_i)).size] }
 
-map = []
-lines.map do |line|
-  map << [1, (line[0].split(" ").map(&:to_i) & line[1].split(" ").map(&:to_i)).size]
+lines.each_with_index do |(cards_number, matches_number), index|
+  cards_number.times { matches_number.times { |match_idx| lines[index + match_idx + 1][0] += 1 } }
 end
 
-map.each_with_index do |line, index|
-  cards_number, matches_number = line
-
-  cards_number.times do
-    matches_number.times do |match_idx|
-      map[index + match_idx + 1][0] += 1
-    end
-  end
-end
-
-puts map.map(&:first).sum
+puts lines.map(&:first).sum
